1141814df2c43c13ba3265419bdc915b086b78c9
postgotmail <postgotmail@gmail.com>
2014-07-02 11:42:57 +0000

    * TUIRibbon now inherits from TCustomControl to provide a basic designer support for the ribbon component. * Added package UIRibbonPackage which provides TUIRibbon as a design time component. * Extracted relevant code to prevent black caption bar on Aero themes from TUIRibbonForm and moved it to TUIRibbon component. * TUIRibbon now provides properties ResourceName, ResourceInstance and event OnCommandCreate to improve design time support. * Added "High Level" example of "SimpleRibbon" project which makes use of the new design time component.

---
+++ Samples/High Level/01 Simple Ribbon/FMain.pas
@@ -0,0 +1,24 @@
+unit FMain;
+
+interface
+
+uses
+  Forms, UIRibbon, System.Classes, Vcl.Controls;
+
+type
+  TFormMain = class(TForm)
+    UIRibbon1: TUIRibbon;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FormMain: TFormMain;
+
+implementation
+
+{$R *.dfm}
+
+end.

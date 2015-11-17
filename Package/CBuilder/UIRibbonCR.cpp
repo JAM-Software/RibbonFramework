df43b623d457377ac6c7c8b2e9a577cd83648b13
Roman Kassebaum <roman.kassebaum@kdv-dt.de>
2015-11-18 00:47:52 +0100

    There was a small issue under Win64.

--- Package/CBuilder/UIRibbonCR.cpp
+++ Package/CBuilder/UIRibbonCR.cpp
@@ -3,7 +3,11 @@
 #include <System.hpp>
 #pragma hdrstop
 #pragma package(smart_init)
+#ifdef _WIN64
+#pragma comment(lib, "Shlwapi.a")
+#else
 #pragma comment(lib, "Shlwapi.lib")
+#endif
 //---------------------------------------------------------------------------
 
 //   Package-Quelltext.

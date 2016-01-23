f3d87dfb9cc2313654908f5fd99454ef39f0eeb1
Joachim Marder <joachim.marder@gmail.com>
2016-01-23 15:18:10 +0100

    Fixed issue #24: Ribbon designer freezes during build

--- Designer/Classes/RibbonCompiler.pas
+++ Designer/Classes/RibbonCompiler.pas
@@ -225,14 +225,8 @@ begin
         begin
           PeekNamedPipe(ReadPipe, @Buffer[0], SizeOf(Buffer) div 10, @BytesRead, @BytesAvail, @BytesLeft); // Read smaller chunks for continuous output
           LogBuffer;
-          Break;
         end;
-
-        BytesRead := 0;
-        if (not ReadFile(ReadPipe, Buffer[0], SizeOf(Buffer) - 1, BytesRead, nil)) then
-          Break;
-        LogBuffer;
-      until (BytesRead = 0);
+      until AppRunning = WAIT_OBJECT_0;
       Result := GetExitCodeProcess(ProcessInfo.hProcess, ExitCode);
       Result := Result and (ExitCode = 0);
     finally

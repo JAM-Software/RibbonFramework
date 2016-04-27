cea2ffa35bad0d3f2c08b34c2ce8934666ea5c98
Joachim Marder <joachim.marder@gmail.com>
2016-04-27 20:58:44 +0200

    Turned on mem leak reporting.

--- Samples/High Level/07 Text Pad with Action List/TextPad.dpr
+++ Samples/High Level/07 Text Pad with Action List/TextPad.dpr
@@ -3,13 +3,19 @@ program TextPad;
 {$R 'Galleries.res' 'Galleries.rc'}
 
 uses
-  Forms,
+   madExcept,
+  madLinkDisAsm,
+  madListHardware,
+  madListProcesses,
+  madListModules,
+ Forms,
   FMain in 'FMain.pas' {FormMain},
   RibbonMarkup in 'Ribbon\RibbonMarkup.pas';
 
 {$R *.res}
 
 begin
+  ReportMemoryLeaksOnShutdown := True;
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
   Application.CreateForm(TFormMain, FormMain);
